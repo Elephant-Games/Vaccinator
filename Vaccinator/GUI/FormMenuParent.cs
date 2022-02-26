3076df7bc82f067b4a2920f8fd3d064b70d93a8e
matthew-shtyasek <shtyasek2003@mail.ru>
2022-02-26 09:13:22 +0000

    For local backup

---
+++ Vaccinator/GUI/FormMenuParent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vaccinator.Fonts;
+
+namespace Vaccinator.GUI {
+    public partial class FormMenuParent : FormMain {
+        public FormMenuParent() {
+            InitializeComponent();
+
+            foreach (Control control in this.Controls)
+                control.Font = Fonts.Font.GetFont(FontName.MENU_TEXT);
+        }
+    }
+}
