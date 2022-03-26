72a0918faa0204c81291fa53148ae3f148e5326a
Yana <ba.ya.na@mail.ru>
2022-03-26 22:57:33 +0500

    Create Bonus class

---
+++ Vaccinator/Game/GameObjects/Bonus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaccinator.Game.GameObjects {
+    abstract class Bonus : GameObject {
+        protected byte timeOfAct;
+        private byte existenceTime;
+
+        public void Action(player: Player) {
+
+        }
+    }
+}
